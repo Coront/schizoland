e5fbb8ad12911eec43f2a082b5dee49d1d554ce3
Tamp-y <64803045+Tamp-y@users.noreply.github.com>
2022-04-17 01:20:35 -0500

    Poison - Severe Status

--- gamemode/tables/effects.lua
+++ gamemode/tables/effects.lua
@@ -20,6 +20,14 @@ AS.AddEffect( "poison", {
     type = "negative",
 } )
 
+AS.AddEffect( "poisonsevere", {
+    name = "Poison - Severe",
+    desc = "Lose health quickly over time.",
+    icon = "icon16/bug.png",
+    color = Color( 205, 180, 0),
+    type = "negative",
+} )
+
 AS.AddEffect( "healingsickness", {
     name = "Healing Sickness",
     desc = "Cannot use healing items.",
@@ -85,10 +93,11 @@ end)
 
 hook.Add( "Think", "AS_Effect_Poison", function()
     for k, v in pairs( player.GetAll() ) do
-        if not v:HasStatus( "poison" ) then continue end --Skip every player that doesn't have this effect.
+        if not v:HasStatus( "poison" ) and not v:HasStatus( "poisonsevere" ) then continue end --Skip every player that doesn't have this effect.
         if CurTime() < (v.Poison_NextTick or 0) then continue end --Still waiting for tick.
+        local severepoison = v:HasStatus( "poisonsevere" ) and true or false
 
-        v.Poison_NextTick = CurTime() + 0.99
+        v.Poison_NextTick = CurTime() + (severepoison and 0.39 or 0.99)
         if ( SERVER ) then
             v:TakeDamage( 1 )
         end
