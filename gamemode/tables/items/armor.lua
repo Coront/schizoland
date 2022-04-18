0a08b91b70eb9f04a3a18168236fbd7abb994774
Tamp-y <Tamp-y>
2022-04-18 01:05:54 -0500

    Armor Balancing

--- gamemode/tables/items/armor.lua
+++ gamemode/tables/items/armor.lua
@@ -7,8 +7,8 @@ AS.AddBaseItem("armor_makeshift", {
     value = 35,
     weight = 2,
     armor = {
-        ["movemult"] = 0.95, --Movespeed Mult
-        [DMG_BULLET] = 8, --Bullets
+        ["movemult"] = 1, --Movespeed Mult
+        [DMG_BULLET] = 10, --Bullets
         [DMG_SLASH] = 12, --Melee
         [DMG_BURN] = 9, --Fire
         [DMG_ENERGYBEAM] = 5, --Pulse
@@ -33,7 +33,7 @@ AS.AddBaseItem("armor_bandit", {
     weight = 2,
     armor = {
         ["movemult"] = 0.95, --Movespeed Mult
-        [DMG_BULLET] = 7, --Bullets
+        [DMG_BULLET] = 15, --Bullets
         [DMG_SLASH] = 14, --Melee
         [DMG_BURN] = 11, --Fire
         [DMG_ENERGYBEAM] = 4, --Pulse
@@ -48,31 +48,31 @@ AS.AddBaseItem("armor_bandit", {
     },
 })
 
-AS.AddBaseItem("armor_combat", {
-    name = "Combat Armor",
-    desc = "A heavy set of armor made of kevlar and a couple other materials. Useful for stopping several incoming bullets.",
+AS.AddBaseItem("armor_recon", {
+    name = "Recon Armor",
+    desc = "A very light set of armor with technologies that improve the wearer's mobility.",
     category = "armor",
-    model = "models/items/armor/combat.mdl",
-    wep = "armor_combat",
-    value = 35,
-    weight = 2,
+    model = "models/items/armor/recon.mdl",
+    wep = "armor_recon",
+    color = Color( 120, 40, 40 ),
+    value = 200,
+    weight = 4.5,
     armor = {
-        ["movemult"] = 0.8, --Movespeed Mult
-        [DMG_BULLET] = 38, --Bullets
-        [DMG_SLASH] = 52, --Melee
-        [DMG_BURN] = 10, --Fire
-        [DMG_ENERGYBEAM] = 30, --Pulse
-        [DMG_BLAST] = 40, --Explosive
+        ["movemult"] = 1.2,
+        [DMG_BULLET] = 25,
+        [DMG_SLASH] = 24,
+        [DMG_BURN] = 18,
+        [DMG_ENERGYBEAM] = 18,
+        [DMG_BLAST] = 20,
     },
     hidden = true,
-    class = "scavenger",
+    class = "mercenary",
     craft = {
-        ["misc_scrap"] = 350,
-        ["misc_smallparts"] = 200,
-        ["misc_chemical"] = 300,
-        ["misc_hide_antlion"] = 4,
+        ["misc_scrap"] = 250,
+        ["misc_smallparts"] = 300,
+        ["misc_chemical"] = 375,
+        ["misc_hide_antlion"] = 3,
         ["misc_hide_guard"] = 2,
-        ["misc_heavyplate"] = 1,
     },
 })
 
@@ -87,10 +87,10 @@ AS.AddBaseItem("armor_combine", {
     weight = 4.5,
     armor = {
         ["movemult"] = 0.85,
-        [DMG_BULLET] = 30,
+        [DMG_BULLET] = 35,
         [DMG_SLASH] = 25,
         [DMG_BURN] = 15,
-        [DMG_ENERGYBEAM] = 21,
+        [DMG_ENERGYBEAM] = 39,
         [DMG_BLAST] = 45,
     },
     hidden = true,
@@ -104,30 +104,30 @@ AS.AddBaseItem("armor_combine", {
     },
 })
 
-AS.AddBaseItem("armor_recon", {
-    name = "Recon Armor",
-    desc = "A very light set of armor with technologies that improve the wearer's mobility.",
+AS.AddBaseItem("armor_combat", {
+    name = "Combat Armor",
+    desc = "A heavy set of armor made of kevlar and a couple other materials. Useful for stopping several incoming bullets.",
     category = "armor",
-    model = "models/items/armor/recon.mdl",
-    wep = "armor_recon",
-    color = Color( 120, 40, 40 ),
-    value = 200,
-    weight = 4.5,
+    model = "models/items/armor/combat.mdl",
+    wep = "armor_combat",
+    value = 35,
+    weight = 2,
     armor = {
-        ["movemult"] = 1.2,
-        [DMG_BULLET] = 23,
-        [DMG_SLASH] = 24,
-        [DMG_BURN] = 18,
-        [DMG_ENERGYBEAM] = 18,
-        [DMG_BLAST] = 20,
+        ["movemult"] = 0.8, --Movespeed Mult
+        [DMG_BULLET] = 40, --Bullets
+        [DMG_SLASH] = 52, --Melee
+        [DMG_BURN] = 10, --Fire
+        [DMG_ENERGYBEAM] = 42, --Pulse
+        [DMG_BLAST] = 40, --Explosive
     },
     hidden = true,
-    class = "mercenary",
+    class = "scavenger",
     craft = {
-        ["misc_scrap"] = 250,
-        ["misc_smallparts"] = 300,
-        ["misc_chemical"] = 375,
-        ["misc_hide_antlion"] = 3,
+        ["misc_scrap"] = 350,
+        ["misc_smallparts"] = 200,
+        ["misc_chemical"] = 300,
+        ["misc_hide_antlion"] = 4,
         ["misc_hide_guard"] = 2,
+        ["misc_heavyplate"] = 1,
     },
 })
