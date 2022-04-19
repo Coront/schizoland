d238c3b58eb068c460d9de94fb176adc625df72a
Tamp-y <64803045+Tamp-y@users.noreply.github.com>
2022-04-19 00:46:39 -0500

    IsWorld

--- gamemode/server/stress.lua
+++ gamemode/server/stress.lua
@@ -2,6 +2,7 @@ hook.Add("EntityTakeDamage", "AS_Stress", function( target, dmginfo )
     if not tobool(GetConVar("as_stress"):GetInt()) then return end
     if not target:IsNPC() and not target:IsPlayer() and not target:IsNextBot() then return end
     local attacker = dmginfo:GetAttacker()
+    if attacker:IsWorld() then return end
     local length = attacker:IsPlayer() and target:IsPlayer() and SET.PlyCombatLength or SET.CombatLength
 
     if target:IsPlayer() then
