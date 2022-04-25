dff0344753b59152879da8608a5ffde654632b66
Tamp-y <64803045+Tamp-y@users.noreply.github.com>
2022-04-25 16:34:11 -0500

    Entity Rendering Distance

--- entities/entities/as_workbench/cl_init.lua
+++ entities/entities/as_workbench/cl_init.lua
@@ -2,5 +2,10 @@ include( "shared.lua" )
 include( "menu.lua" )
 
 function ENT:Draw()
-    self:DrawModel()
+    if LocalPlayer():GetPos():Distance(self:GetPos()) < GetConVar("as_entity_renderdist"):GetInt() then
+        self:DrawModel()
+        self:DrawShadow(true)
+    else
+        self:DrawShadow(false)
+    end
 end
