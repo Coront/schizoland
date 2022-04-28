cea2424bd7e8620ae6e2e833e827de13de119946
Tamp-y <Tamp-y>
2022-04-27 22:15:51 -0500

    Separate FastDL for maps

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -93,10 +93,27 @@ local WorkshopFastDL = {
 	["201027715"] = "FA:S 2.0 Alpha SWEPs - U. Rifles",
 	["462119296"] = "FA:S 2.0 Alpha SWEPs - Unused Weapons (Unofficial)",
 	["201027186"] = "FA:S 2.0 Alpha SWEPs - Misc",
-	--Maps
-	["2795340202"] = "r_postnukemetro_dry",
-	["2595136841"] = "RP Asheville",
 }
+
+local WorkshopFastDLMaps = {
+	["2795340202"] = "r_postnukemetro_dry",
+	["2225370566"] = "rp_outercanals_p",
+	["2220973603"] = "rp_mojave_v3_p",
+	["22209959703"] = "rp_cscdesert_v4b2",
+	["2227565470"] = "pn_wastedwaters_beta_2_p",
+	["2220995823"] = "gm_disaster_v3_nodust",
+	["143547520"] = "rp_ineu_pass",
+	["1572373847"] = "gm_boreas", --Event Map
+	["326332456"] = "gm_fork", --Event Map
+	["2595136841"] = "rp_asheville", --Event Map
+}
+
+for k, v in pairs(WorkshopFastDLMaps) do
+	if game.GetMap() == v then
+		WorkshopFastDL[k] = v
+		break
+	end
+end
 
 for k, v in pairs(WorkshopFastDL) do
 	resource.AddWorkshop( k )
