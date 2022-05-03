0526daa50e30cdb5e53a0e01b4ef0dc5bcb13775
Tamp-y <64803045+Tamp-y@users.noreply.github.com>
2022-05-03 05:31:29 -0500

    Pickup Delays for specific tools

--- gamemode/server/entities.lua
+++ gamemode/server/entities.lua
@@ -20,10 +20,21 @@ util.AddNetworkString("as_tool_pickup")
 net.Receive("as_tool_pickup", function( _, ply )
 	local ent = net.ReadEntity()
 
+	if ent.PickupDelay and ent.PickupDelayActive then ply:ChatPrint("You are already attempting to pick this up, please wait.") return end
 	if not ent:PlayerCanPickUp( ply ) then return end
     local item = FetchToolIDByClass( ent:GetClass() )
 	if not item then AS.LuaError("Attempt to pick up an object with no entity tied, cannot find itemid - " .. ent:GetClass()) return end
 	if ply:GetCarryWeight() + AS.Items[item].weight > ply:MaxCarryWeight() then ply:ChatPrint("You are too overweight to carry this.") return end
 
-	ent:PickedUp( ply, item )
+	if ent.PickupDelay then
+		ply:ChatPrint("This object has a pickup delay. You will pick it up in " .. ent.PickupDelay .. " seconds.")
+		ent.PickupDelayActive = true
+		timer.Simple( ent.PickupDelay, function()
+			if IsValid( ent ) then
+				ent:PickedUp( ply, item )
+			end
+		end)
+	else
+		ent:PickedUp( ply, item )
+	end
 end)
