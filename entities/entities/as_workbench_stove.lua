c2fb8aa3ce1e2ca7c99e3662a2abf1e8b1bad842
Tamp-y <64803045+Tamp-y@users.noreply.github.com>
2022-05-13 21:11:37 -0500

    Stove Crafting

--- entities/entities/as_workbench_stove.lua
+++ entities/entities/as_workbench_stove.lua
@@ -9,7 +9,12 @@ ENT.Spawnable		= false
 ENT.Model = "models/props_c17/furnitureStove001a.mdl"
 
 ENT.CraftTable = {
-    ["food_clean_water"] = 1,
-    ["food_coffee"] = 2,
-    ["food_soup"] = 3,
+    ["food_cooked_beans"] = 1,
+    ["food_purified_water"] = 2,
+    ["food_cooked_meat"] = 3,
+    ["food_cooked_antlion"] = 4,
+    ["food_cooked_guard"] = 5,
+    ["food_takeout"] = 6,
+    ["food_soup"] = 7,
+    ["food_coffee"] = 8,
 }
