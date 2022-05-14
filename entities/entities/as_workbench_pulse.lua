5f1f86dea79035d41ffac5ba5f2bfd7e6c0bc1d1
Tamp-y <64803045+Tamp-y@users.noreply.github.com>
2022-05-13 21:11:29 -0500

    Pulse Station Crafting

--- entities/entities/as_workbench_pulse.lua
+++ entities/entities/as_workbench_pulse.lua
@@ -13,4 +13,10 @@ ENT.CraftTable = {
     ["wep_pulserifle"] = 2,
     ["wep_pulselmg"] = 3,
     ["wep_pulsesniper"] = 4,
+    ["med_bloodbag"] = 5,
+    ["med_surgery"] = 6,
+    ["med_antidote"] = 7,
+    ["med_adrenaline"] = 8,
+    ["med_painkiller"] = 9,
+    ["med_suppression"] = 10,
 }
