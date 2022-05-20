1256c5825ed8e1d299c0054c823f68d868f52275
Tamp-y <64803045+Tamp-y@users.noreply.github.com>
2022-05-20 17:22:48 -0500

    Removed Old Storage

--- entities/entities/as_storage/shared.lua
+++
@@ -1,7 +0,0 @@
-ENT.Type 			= "anim"
-ENT.Base 			= "base_entity"
-ENT.PrintName		= "Storage"
-ENT.Author			= "Tampy"
-ENT.Purpose			= "A storage. You can put your belongings in it."
-ENT.Category		= "Aftershock"
-ENT.Spawnable		= false --true
