2a5fed49c47c7ad83712b1d2f5cc64b5ece5537b
Tamp-y <64803045+Tamp-y@users.noreply.github.com>
2022-06-02 18:12:58 -0500

    bug

--- entities/entities/as_workbench/init.lua
+++ entities/entities/as_workbench/init.lua
@@ -45,10 +45,10 @@ net.Receive("as_workbench_craftitem", function( _, ply )
 	if not IsValid(ent) then return end
 	if ent.Base != "as_workbench" then return end
 	if ply:GetPos():Distance(ent:GetPos()) > 300 then ply:ChatPrint("You're too far.") return end
-
+	
 	local item = net.ReadString()
 	local amt = net.ReadUInt( NWSetting.ItemCraftBits )
-
+	
 	if not AS.Items[item] then ply:ChatPrint("This isnt a valid item.") ply:ResyncInventory() return end --item doesnt exist
 	if not ent.CraftTable then ply:ChatPrint("This isnt a workbench.") ply:ResyncInventory() return end
     if not ent.CraftTable[item] then ply:ChatPrint("You cannot make this item at this workbench.") ply:ResyncInventory() return end --Item exists but is not craftable at this workbench
