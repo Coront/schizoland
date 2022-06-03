5d74a717fceaaa47584bb37f9a5bb5fd18f8cd80
Tamp-y <64803045+Tamp-y@users.noreply.github.com>
2022-06-03 10:22:23 -0500

    Fix

--- entities/weapons/gmod_tool/stools/as_asnpcspawner.lua
+++ entities/weapons/gmod_tool/stools/as_asnpcspawner.lua
@@ -23,7 +23,7 @@ local Entities = {
     {class = "npc_as_super", name = "Super Soldier"},
 }
 
-TOOL.ClientConVar["NPC"] = "" --NPC Base
+TOOL.ClientConVar["NPC"] = Entities[1].class --NPC Base
 --Stats
 TOOL.ClientConVar["Name"] = "" --Name
 TOOL.ClientConVar["Model"] = "" --Model
@@ -32,7 +32,7 @@ TOOL.ClientConVar["Health"] = 100 --Default Health
 TOOL.ClientConVar["Guard"] = 0 --Guard spawn position
 TOOL.ClientConVar["GuardForce"] = 0 --Force hold (never move, even with enemy)
 --Relationship
-TOOL.ClientConVar["Group"] = "custom" --Alliance Group
+TOOL.ClientConVar["Group"] = "" --Alliance Group
 TOOL.ClientConVar["Friendly"] = 1 --Friendly to players
 TOOL.ClientConVar["AttackAttackers"] = 1 --Attack All Attackers
 TOOL.ClientConVar["Alert"] = 1 --Alert allies when attacked
@@ -47,15 +47,51 @@ local function BoolToInt( bool )
 	end
 end
 
-function TOOL:LeftClick()
+function TOOL:LeftClick( trace )
     if CLIENT then return false end --False because silencing toolgun sound
 
     local class = self:GetClientInfo( "NPC" )
+    local name = self:GetClientInfo( "Name" )
+    local model = self:GetClientInfo( "Model" )
+    local hp = self:GetClientInfo( "Health" )
+    local group = self:GetClientInfo( "Group" )
+    local guard = self:GetClientInfo( "Guard" )
+    local guardf = self:GetClientInfo( "GuardForce" )
+    local friendly = self:GetClientInfo( "Friendly" )
+    local attackattackers = self:GetClientInfo( "AttackAttackers" )
+    local alert = self:GetClientInfo( "Alert" )
+    local items = self:GetClientInfo( "Items" )
 
     local ply = self:GetOwner()
     local ent = ents.Create(class)
     ent:SetPos( trace.HitPos + Vector(0, 0, 10) )
     ent:Spawn()
+
+    if name != "" then
+        ent:SetNWString("Name", name)
+    end
+    if model != "" then
+        ent.Model = model
+    end
+    ent:SetHealth( hp )
+    ent:SetMaxHealth( hp )
+    if group != "" then
+        ent.Group = group
+    end
+    ent.Guarding = tobool( guard )
+    ent.GuardingForceHold = tobool( guardf )
+    ent.Friendly = tobool( friendly )
+    ent.AttackAllAttackers = tobool( attackattackers )
+    if ent.AttackAllAttackers then
+        ent.AttackAllAttackersTolerance = ent.AttackAllAttackersTolerance <= 0 and 1 or ent.AttackAllAttackersTolerance
+    end
+    if not tobool( alert ) then
+        ent.CommunicateDistance = 1
+    end
+    if not tobool( items ) then
+        ent.LootTable = nil
+    end
+
 
     undo.Create("as_asnpcspawner")
         undo.AddEntity( ent )
@@ -76,17 +112,79 @@ function TOOL.BuildCPanel(panel)
 
     class = vgui.Create("DComboBox", panel)
 	class:SetSize( panel:GetWide(), 20 )
-	class:SetValue("Test")
+	class:SetValue( Entities[1].name )
 	for k, v in pairs( Entities ) do
 		class:AddChoice(v.name)
 	end
     function class:OnSelect( panel, index, value )
         for k, v in pairs( Entities ) do
-            if v.name == value then
-                --do this
+            if v.name == index then
+                RunConsoleCommand( "as_asnpcspawner_npc", v.class )
                 break
             end
         end
     end
 	panel:AddItem(class)
+
+    local name = vgui.Create("DTextEntry")
+	name:SetUpdateOnType(true)
+	name:SetEnterAllowed(true)
+	name:SetConVar("as_asnpcspawner_Name")
+	name:SetValue( GetConVarString("as_asnpcspawner_name") )
+    name:SetPlaceholderText( "Name (Empty for Default)" )
+	panel:AddItem( name )
+
+    local group = vgui.Create("DTextEntry")
+	group:SetUpdateOnType(true)
+	group:SetEnterAllowed(true)
+	group:SetConVar("as_asnpcspawner_group")
+	group:SetValue( GetConVarString("as_asnpcspawner_group") )
+    group:SetPlaceholderText( "Group (Empty for Default)" )
+	panel:AddItem( group )
+
+    local model = vgui.Create("DTextEntry")
+	model:SetUpdateOnType(true)
+	model:SetEnterAllowed(true)
+	model:SetConVar("as_asnpcspawner_model")
+	model:SetValue( GetConVarString("as_asnpcspawner_model") )
+    model:SetPlaceholderText( "Model (Empty for Default)" )
+	panel:AddItem( model )
+
+    panel:AddControl("Slider", {
+        Label = "Health", 
+        Type = "Integer",
+        Min = "1",
+        Max = "100000",
+        Command = "as_asnpcspawner_health"
+	})
+
+    panel:AddControl("CheckBox", {
+		Label = "Guard Position",
+		Command = "as_asnpcspawner_guard"
+	})
+
+    panel:AddControl("CheckBox", {
+		Label = "Force Hold Position (Never Move)",
+		Command = "as_asnpcspawner_guardforce"
+	})
+
+    panel:AddControl("CheckBox", {
+		Label = "Friendly to Players",
+		Command = "as_asnpcspawner_friendly"
+	})
+
+    panel:AddControl("CheckBox", {
+		Label = "Attack all Attackers, regardless of relationship?",
+		Command = "as_asnpcspawner_attackattackers"
+	})
+
+    panel:AddControl("CheckBox", {
+		Label = "Alert when NPC allies are attacked",
+		Command = "as_asnpcspawner_alert"
+	})
+
+    panel:AddControl("CheckBox", {
+		Label = "Drop Items",
+		Command = "as_asnpcspawner_items"
+	})
 end
