8f43f0d3d88c0dc9f92e14bc9381110fc895db14
Tamp-y <64803045+Tamp-y@users.noreply.github.com>
2022-06-09 22:17:37 -0500

    Adjustment

--- gamemode/modules/sh_player/sh_player.lua
+++ gamemode/modules/sh_player/sh_player.lua
@@ -16,7 +16,7 @@ function GM:Move( ply, mv ) --I'm intentionally overriding this so we don't use 
     ply:SetWalkSpeed( movespeed )
     ply:SetSlowWalkSpeed( 75 )
     ply:SetDuckSpeed( 0.4 )
-    ply:SetJumpPower( 200 )
+    ply:SetJumpPower( 225 )
     ply:SetViewOffset( Vector( 0, 0, 61 ) )
     ply:SetViewOffsetDucked( Vector( 0, 0, 35 ) )
 end
