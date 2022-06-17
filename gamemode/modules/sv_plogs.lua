53e7572c48354dd391369c8c0a7d22e85471f877
Tamp-y <64803045+Tamp-y@users.noreply.github.com>
2022-06-16 19:41:26 -0500

    changes

--- gamemode/modules/sv_plogs.lua
+++ gamemode/modules/sv_plogs.lua
@@ -3,6 +3,7 @@ if plogs then
     plogs.Register("Cases", false, Color( 85, 0, 180 ))
     plogs.Register("Craft", true, Color( 175, 180, 0 ))
     plogs.Register("Items", false, Color( 0, 75, 180 ))
+    plogs.Register("Vehicles", false, Color( 0, 75, 180 ))
     plogs.Register("Storage", false, Color( 175, 95, 0 ))
     plogs.Register("Kills (NPC)", false)
     plogs.Register("Admin", true, Color( 175, 25, 0 ))
