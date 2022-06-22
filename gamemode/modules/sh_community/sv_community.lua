c578ccd115483114bb2912d6e7bdf946c3fbfceb
Tamp-y <64803045+Tamp-y@users.noreply.github.com>
2022-06-22 15:38:57 -0500

    More sync issues

--- gamemode/modules/sh_community/sv_community.lua
+++ gamemode/modules/sh_community/sv_community.lua
@@ -454,6 +454,11 @@ function PlayerMeta:DeployLocker()
     ent:SetCommunity( self:GetCommunity(), self:GetCommunityName() )
     ent:Spawn()
     ent:SetPos( self:TracePosFromEyes( 300 ) + Vector( 0, 0, 12 ))
+
+    timer.Simple( 1, function()
+        ent:ResyncCommunity()
+        ent:ResyncInventory()
+    end)
 end
 
 function PlayerMeta:DeployStockpile()
@@ -461,6 +466,11 @@ function PlayerMeta:DeployStockpile()
     ent:SetCommunity( self:GetCommunity(), self:GetCommunityName() )
     ent:Spawn()
     ent:SetPos( self:TracePosFromEyes( 300 ) + Vector( 0, 0, 80 ))
+
+    timer.Simple( 1, function()
+        ent:ResyncCommunity()
+        ent:ResyncResources()
+    end)
 end
 
 -- ██████╗ ██████╗ ███████╗    ██╗      ██████╗  █████╗ ██████╗ ███████╗██████╗
