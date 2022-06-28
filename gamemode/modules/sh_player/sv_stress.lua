eb10ce616bb4d0d431f2e8790ddced7338eabac0
Tamp-y <64803045+Tamp-y@users.noreply.github.com>
2022-06-28 11:40:43 -0500

    Fix here

--- gamemode/modules/sh_player/sv_stress.lua
+++ gamemode/modules/sh_player/sv_stress.lua
@@ -2,7 +2,7 @@ hook.Add("EntityTakeDamage", "AS_Stress", function( target, dmginfo )
     if not tobool(GetConVar("as_stress"):GetInt()) then return end
     if not target:IsPlayer() then return end
     local attacker = dmginfo:GetAttacker()
-    if attacker:IsWorld() or not attacker:IsNPC() or not attacker:IsPlayer() or not attacker:IsNextBot() then return end
+    if attacker:IsWorld() or (not attacker:IsNPC() and not attacker:IsPlayer() and not attacker:IsNextBot()) then return end
     local length = attacker:IsPlayer() and target:IsPlayer() and SET.PlyCombatLength or SET.CombatLength
 
     if target:IsPlayer() then
